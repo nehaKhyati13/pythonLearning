fb8c12ec5f4b107eef64864ef3d18b91c229f117
Kishu Gupt@ <kishugupt@Kishus-Mac-mini.local>
2025-04-12 21:35:23 +0530

    first code for qrgenerator

---
+++ qrcodegenerator.py
@@ -0,0 +1,14 @@
+import qrcode
+import image
+qr = qrcode.QRCode(
+    version = 20, 
+    box_size = 5,
+    border = 5
+)
+
+data = "Hi"
+
+qr.add_data(data)
+qr.make(fit = True)
+img = qr.make_image(fill="red",back_color ="green")
+img.save("qr.png")
